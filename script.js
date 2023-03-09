668bbe7d9a32ecd9adecb986801cbfe6d038e814
aytankhaligli <ayten.xaliqli@gmail.com>
2023-03-10 01:51:41 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+// const title = document.querySelector("h1");
+// // title.textContent = "Thank you!";
+
+// const button = document.querySelectorAll(".number");
+// const activebtn = function (i) {
+//   for (let i = 0; i < button.length; i++) {
+//     button[i].style.backgroundColor = "hsl(25, 97%, 53%)";
+//     button[i].style.color = "white";
+//   }
+// };
+// button[i].addEventListener("click", activebtn(i));
